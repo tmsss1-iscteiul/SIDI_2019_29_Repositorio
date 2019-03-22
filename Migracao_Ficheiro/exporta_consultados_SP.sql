b4511cf8b0bb37e721a7c1aab32edeee035bafd3
absta-iscteiulpt <43684254+absta-iscteiulpt@users.noreply.github.com>
2019-03-22 22:55:06 +0000

    Update exporta_consultados_SP.sql

--- Migracao_Ficheiro/exporta_consultados_SP.sql
+++ Migracao_Ficheiro/exporta_consultados_SP.sql
@@ -1,13 +1,13 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `exporta_consultados`()
 BEGIN
 if ( (SELECT MIN(id) FROM medicoes_consultados_log WHERE Flag_Migracao =0) is not null)
-then set @command = 'select * from medicoes_consultados_log where ID>=(SELECT MIN(id) FROM medicoes_consultados_log WHERE Flag_Migracao =0)';
+then set @command = 'select DataHoraMedicao, DataHora_LOG, ID, IDCultura_VariaveisMedidas, IDMedicoes_LOG, IDVariaveis_VariaveisMedidas, NumeroMedicao, Operacao_LOG, Utilizador_LOG, ValorMedicao from medicoes_consultados_log where ID>=(SELECT MIN(id) FROM medicoes_consultados_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM medicoes_consultados_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
 set @conc = concat (@command, ' into outfile "', "C:/xampp/CONSULTADOS_", @command_dois, @nome_ficheiro);
 
-prepare s1 from @conc;
-execute s1;
+prepare q from @conc;
+execute q;
 
 end if;
 END
