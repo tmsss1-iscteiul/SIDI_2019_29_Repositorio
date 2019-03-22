9008533e6199c49fa440ebf474152e0c29ff2b2f
absta-iscteiulpt <43684254+absta-iscteiulpt@users.noreply.github.com>
2019-03-22 22:57:34 +0000

    Update exporta_medicoes_SP.sql

--- Migracao_Ficheiro/exporta_medicoes_SP.sql
+++ Migracao_Ficheiro/exporta_medicoes_SP.sql
@@ -1,13 +1,13 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `exporta_medicoes`()
 BEGIN
 if ( (SELECT MIN(id) FROM medicoes_log WHERE Flag_Migracao =0) is not null)
-then set @command = 'select * from medicoes_log where ID>=(SELECT MIN(id) FROM medicoes_log WHERE Flag_Migracao =0)';
+then set @command = 'select DataHora, DataHoraMedicao_NEW, DataHoraMedicao_OLD, ID, IDCultura_VariaveisMedidas_NEW, IDCultura_VariaveisMedidas_OLD, IDVariaveis_VariaveisMedidas_NEW, IDVariaveis_VariaveisMedidas_OLD, NumeroMedicao_NEW, NumeroMedicao_OLD, Operacao, Utilizador, ValorMedicao_NEW, ValorMedicao_OLD from medicoes_log where ID>=(SELECT MIN(id) FROM medicoes_log WHERE Flag_Migracao =0)';
 set @command_dois =(select MIN(id) FROM medicoes_log where Flag_Migracao =0);
 set @nome_ficheiro = '.csv" FIELDS TERMINATED BY "," ENCLOSED BY "\'\" LINES TERMINATED BY "\n" ;';
 set @conc = concat (@command, ' into outfile "', "C:/xampp/MEDICOES_", @command_dois, @nome_ficheiro);
 
-prepare s1 from @conc;
-execute s1;
+prepare q from @conc;
+execute q;
 
 end if;
 END
