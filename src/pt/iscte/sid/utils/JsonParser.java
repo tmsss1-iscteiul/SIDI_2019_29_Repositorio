96b06b961fbb66aa734b75f6da63f4c9dfc2506e
jfnfs <jfnfs@iscte-iul.pt>
2019-05-05 10:42:05 +0100

    alterações JSON

--- src/pt/iscte/sid/utils/JsonParser.java
+++ src/pt/iscte/sid/utils/JsonParser.java
@@ -10,10 +10,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 public class JsonParser {
 
 	public String[] SensorMessageParser(String message) {
-		String[] array = new String[6];
-		JSONObject obj = new JSONObject(str);
-		JSONArray arr = obj.getJSONArray("number");
-		for (int i = 0; i < arr.length(); i++)
-		    System.out.println(arr.getInt(i));
+		return null;
 	}
 }
