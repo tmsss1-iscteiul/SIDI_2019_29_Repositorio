0c443fe0114616f6f185f06ab8bb42ec2be22ff2
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-05-13 23:25:03 +0100

    Update Testes

--- src/pt/iscte/es/testsuit/AllTests.java
+++ src/pt/iscte/es/testsuit/AllTests.java
@@ -29,37 +29,68 @@ public class AllTests {
 	@Test
 	public void test() throws Exception {
 		Login login = new Login();
-		Connection c = login.getConnection("root", "");
+		Connection c = login.getConnection(investigador, investigadorPass);
 		assertNotEquals(null, c);
-		InvestigadorAPP in = new InvestigadorAPP(c, "root");
+		InvestigadorAPP in = new InvestigadorAPP(c, investigador);
 		assertNotEquals(null, in);
 		
-		Connection c2 = login.getConnection("admin", "12345");
+		Connection c2 = login.getConnection(admin, passAdmin);
 		assertNotEquals(null, c2);
-		AdminAPP ad = new AdminAPP(c2, "admin");
+		AdminAPP ad = new AdminAPP(c2, admin);
 		assertNotEquals(null, ad);
 	
 		//Investigador
-		InvestigadorSQL cmd = new InvestigadorSQL(c, "root");
-		ArrayList<Cultura> cultura = cmd.getCultura();
+		InvestigadorSQL investigadorSQL = new InvestigadorSQL(c, investigador);
+		//getCultura
+		ArrayList<Cultura> cultura = investigadorSQL.getCultura();
 		assert(!cultura.isEmpty());
-		ArrayList<Variavel> variavel = cmd.getVariaveis();
+		//getVariaveis
+		ArrayList<Variavel> variavel = investigadorSQL.getVariaveis();
 		assert(!variavel.isEmpty());
-		ArrayList<Cultura> cultura2 = cmd.getCulturasSemVariaveis();
+		//getCulturasSemVariaveis
+		ArrayList<Cultura> cultura2 = investigadorSQL.getCulturasSemVariaveis();
 		assert(!cultura2.isEmpty());
-		int id = cmd.buscaIDCultura("CulturaNova");
+		//buscaIDCultura
+		int id = investigadorSQL.buscaIDCultura("CulturaNova");
 		assertEquals(7, id);
-		assertEquals("root@localhost", cmd.buscaEmail());
-		assertEquals(7, cmd.idCultura());
-		assertEquals(1, cmd.buscaIDVariavel("Variavel de Teste"));
-		variavel = cmd.getVariaveisAssociadasCultura(1);
+		assertEquals("root@localhost", investigadorSQL.buscaEmail());
+		assertEquals(7, investigadorSQL.idCultura());
+		assertEquals(1, investigadorSQL.buscaIDVariavel("Variavel de Teste"));
+		variavel = investigadorSQL.getVariaveisAssociadasCultura(1);
 		assertNotEquals(null, variavel);
-		variavel = cmd .getVariavelEspecifica(1);
+		variavel = investigadorSQL .getVariavelEspecifica(1);
 		assertNotEquals(null, variavel);
 		
 		//Admin
-		AdminSQL cm = new AdminSQL(c2, "admin");
-		
+		AdminSQL cmd = new AdminSQL(c2, admin);
+		//getInvestigador
+		ArrayList<Investigador> investigador = cmd.getInvestigador();
+		assertNotEquals(null, investigador);
+		//inserirInvestigador
+		int size = investigador.size();
+		cmd.inserirInvestigador("emailTeste123@Teste", "Nome Teste", "Categori Teste");
+		investigador = cmd.getInvestigador();
+		size++;
+		assertEquals(size, investigador.size());
+		//deleteInvestigador
+		cmd.deleteInvestigador("emailTeste123@Teste");
+		investigador = cmd.getInvestigador();
+		size--;
+		assertEquals(size, investigador.size());
+		//getVariaveis
+		variavel = cmd.getVariaveis();
+		assertNotEquals(null, variavel);
+		//inserirVariavel
+		size = variavel.size();
+		cmd.inserirVariavel("VariavelTesteNova");
+		size++;
+		variavel = cmd.getVariaveis();
+		assertEquals(size, variavel.size());
+		//deleteVariavel
+		cmd.deleteVariavel(variavel.get(variavel.size()-1).getID());
+		size--;
+		variavel = cmd.getVariaveis();
+		assertEquals(size, variavel.size());
 	}
 
 }
