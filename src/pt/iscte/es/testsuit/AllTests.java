e11d390885ec92073e13f66f3d3c518c6c878dd6
Marcos Silva <44324057+mpcts-iscteiul@users.noreply.github.com>
2019-05-13 21:54:03 +0100

    Update AllTests.java

--- src/pt/iscte/es/testsuit/AllTests.java
+++ src/pt/iscte/es/testsuit/AllTests.java
@@ -1,11 +1,44 @@
 package pt.iscte.es.testsuit;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
+import static org.junit.Assert.*;
 
-@RunWith(Suite.class)
-@SuiteClasses({})
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import pt.iscte.es.admin.AdminAPP;
+import pt.iscte.es.admin.AdminSQL;
+import pt.iscte.es.investigador.InvestigadorAPP;
+import pt.iscte.es.investigador.InvestigadorSQL;
+import pt.iscte.es.login.Login;
+import pt.iscte.es.objetos.Cultura;
+import pt.iscte.es.objetos.Variavel;
+
 public class AllTests {
 
+	@Test
+	public void test() throws Exception {
+		Login login = new Login();
+		Connection c = login.getConnection("root", "");
+		assertNotEquals(null, c);
+		InvestigadorAPP in = new InvestigadorAPP(c, "root");
+		assertNotEquals(null, in);
+		
+		Connection c2 = login.getConnection("admin", "12345");
+		assertNotEquals(null, c2);
+		AdminAPP ad = new AdminAPP(c2, "admin");
+		assertNotEquals(null, ad);
+	
+		//Investigador
+		InvestigadorSQL cmd = new InvestigadorSQL(c, "root");
+		ArrayList<Cultura> cultura = cmd.getCultura();
+		ArrayList<Variavel> variavel = cmd.getVariaveis();
+		
+		//Admin
+		AdminSQL cm = new AdminSQL(c2, "admin");
+		
+	}
+
 }
+
